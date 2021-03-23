004f36298a0250a2e3dc1d7d6e931d673fb4a028
StorozhukDev <Storozhuck@gmail.com>
2021-03-23 03:11:19 +0200

    lesson 2 homework

---
+++ HomeWork/src/com/storozhuk/lesson2/task1.java
@@ -0,0 +1,21 @@
+package com.storozhuk.lesson2;
+
+/*
+ * Counting the average of two numbers
+ */
+
+public class task1 {
+    public static void main(String[] args) {
+        if(args.length != 2) {
+            System.out.println("Invalid arguments count: " + args.length);
+            return;
+        }
+
+        double val1 = Double.parseDouble(args[0]);
+        double val2 = Double.parseDouble(args[1]);
+
+        System.out.println("Value #1: " + val1);
+        System.out.println("Value #2: " + val2);
+        System.out.println("\nAverage: " + (val1+val2)/2);
+    }
+}
---
+++ HomeWork/src/com/storozhuk/lesson2/task2.java
@@ -0,0 +1,25 @@
+package com.storozhuk.lesson2;
+
+/*
+ * Counting the average of any numbers
+ */
+
+public class task2 {
+    public static void main(String[] args) {
+        if(args.length < 2) {
+            System.out.println("Too few arguments.");
+            return;
+        }
+
+        double argsSum = 0d;
+        int argCount = 0;
+
+        for(String arg : args) {
+            argCount++;
+            argsSum += Double.parseDouble(arg);
+            System.out.printf("Value #%d: %s\n", argCount, arg);
+        }
+
+        System.out.println("\nAverage: " + argsSum/argCount);
+    }
+}
