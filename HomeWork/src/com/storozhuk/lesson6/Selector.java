19dc48560715b781f5c5e7c5744f08162c8113e3
StorozhukDev <Storozhuck@gmail.com>
2021-04-06 20:44:41 +0300

    lesson 6 homework

---
+++ HomeWork/src/com/storozhuk/lesson6/Selector.java
@@ -0,0 +1,8 @@
+package com.storozhuk.lesson6;
+
+interface Selector {
+    void showCityCallsTimeGt(long time);
+    void showLongDistCalls();
+    void showWithAlphabetSort();
+    void showTop10Internet();
+}
