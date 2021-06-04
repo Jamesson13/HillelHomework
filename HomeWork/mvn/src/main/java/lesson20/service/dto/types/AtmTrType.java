c898d52b392a3bfdd46b0c3b834b5c85b09101d6
StorozhukDev <Storozhuck@gmail.com>
2021-06-04 14:52:04 +0300

    lesson 20 homework

---
+++ HomeWork/mvn/src/main/java/lesson20/service/dto/types/AtmTrType.java
@@ -0,0 +1,11 @@
+package lesson20.service.dto.types;
+
+/**
+ * Enumeration  AtmTrType
+ *
+ * Possible types of transactions for ATM.
+ */
+public enum AtmTrType {
+    WITHDRAWAL,
+    REPLENISHMENT
+}
